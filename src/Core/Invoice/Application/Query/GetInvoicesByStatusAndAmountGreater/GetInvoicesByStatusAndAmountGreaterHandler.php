5da357850c5bc6ecff98d668c565f9ab1c67c82a
dr <damian.rakowski12@o2.pl>
2024-11-24 08:42:24 +0100

    There is an error in the system. CLI Command "app:invoice:get-by-status-and-amount" for some reason returns all new invoices ignoring the status and amount argument. Find the causes and solve this problem

--- src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
+++ src/Core/Invoice/Application/Query/GetInvoicesByStatusAndAmountGreater/GetInvoicesByStatusAndAmountGreaterHandler.php
@@ -5,21 +5,21 @@ namespace App\Core\Invoice\Application\Query\GetInvoicesByStatusAndAmountGreater
 use App\Core\Invoice\Application\DTO\InvoiceDTO;
 use App\Core\Invoice\Domain\Invoice;
 use App\Core\Invoice\Domain\Repository\InvoiceRepositoryInterface;
-use App\Core\Invoice\Domain\Status\InvoiceStatus;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class GetInvoicesByStatusAndAmountGreaterHandler
 {
     public function __construct(
-        private readonly InvoiceRepositoryInterface $invoiceRepository
-    ) {}
+        private readonly InvoiceRepositoryInterface $invoiceRepository,
+    ) {
+    }
 
     public function __invoke(GetInvoicesByStatusAndAmountGreaterQuery $query): array
     {
         $invoices = $this->invoiceRepository->getInvoicesWithGreaterAmountAndStatus(
             $query->amount,
-            InvoiceStatus::CANCELED
+            $query->invoiceStatus
         );
 
         return array_map(function (Invoice $invoice) {
--- src/Core/Invoice/UserInterface/Cli/GetInvoices.php
+++ src/Core/Invoice/UserInterface/Cli/GetInvoices.php
@@ -5,6 +5,7 @@ namespace App\Core\Invoice\UserInterface\Cli;
 use App\Common\Bus\QueryBusInterface;
 use App\Core\Invoice\Application\DTO\InvoiceDTO;
 use App\Core\Invoice\Application\Query\GetInvoicesByStatusAndAmountGreater\GetInvoicesByStatusAndAmountGreaterQuery;
+use App\Core\Invoice\Domain\Status\InvoiceStatus;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,9 +25,25 @@ class GetInvoices extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $invoices = $this->bus->dispatch(new GetInvoicesByStatusAndAmountGreaterQuery(
-            $input->getArgument('amount')
-        ));
+        $status = InvoiceStatus::tryFrom(strtolower($input->getArgument('status')));
+
+        if (null === $status) {
+            $output->writeln(
+                sprintf(
+                    '<error>Błędny status dozwolone wartości to %s</error>',
+                    join(',', array_column(InvoiceStatus::cases(), 'value'))
+                )
+            );
+
+            return Command::INVALID;
+        }
+
+        $invoices = $this->bus->dispatch(
+            new GetInvoicesByStatusAndAmountGreaterQuery(
+                $input->getArgument('amount'),
+                $status
+            )
+        );
 
         /** @var InvoiceDTO $invoice */
         foreach ($invoices as $invoice) {
